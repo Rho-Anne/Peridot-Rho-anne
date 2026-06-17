a83a99761d5b2a263f516e71fd52939173298c5d
Rho-Anne <rhoannecarlon607@gmail.com>
2026-06-17 14:39:00 +0800

    secondcommit

--- Basic_Structure_and_Syntax/HelloWorld.java
+++ Basic_Structure_and_Syntax/HelloWorld.java
@@ -2,24 +2,43 @@ package Basic_Structure_and_Syntax;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        // System.out.println
+        // (1234567890101111l);
+        // System.out.println
+        // ("Welcome to Java programming.");
+        // System.out.println
+        // ("I am Rho-Anne.");
+        // System.out.println("12- Peridot.");
+        // System.out.println
+        // ("Hello, Sir Seraspe!");
+
+        System.out.println 
+        ("Addition" ); //This is addition
+        System.out.print
+        ("10 + 5 = ");  
         System.out.println
-        (1234567890101111l);
-        System.out.println
-        ("Welcome to Java programming.");
-        System.out.println
-        ("I am Rho-Anne.");
-        System.out.println("12- Peridot.");
-        System.out.println
-        ("Hello, Sir Seraspe!");
+        (10 + 5);
 
         System.out.println
-        ("Addition:" + (5 + 10) );   
+        ("Subtraction" ); //This is subtraction
+        System.out.print
+        ("20 - 8 = ");  
         System.out.println
-        ("Subtraction:" + (20 - 8) );
+        (20 - 8);
+
         System.out.println
-        ("Multiplication:" + (4 * 6) );
+        ("Multiplication" ); //this is multiplication
+        System.out.print
+        ("4 * 6 = ");  
         System.out.println
-        ("Division:" + (15 / 3) );
+        (4 * 6); 
+
+        System.out.println
+        ("Division" ); //This is division
+        System.out.print
+        ("15 / 3 = ");  
+        System.out.println
+        (15 / 3);
 
 
         
