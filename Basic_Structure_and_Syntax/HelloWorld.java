0cc82928e3458bcbd175a76e9ad3baf192e0e977
Rho-Anne <rhoannecarlon607@gmail.com>
2026-06-17 14:16:51 +0800

    calculator

--- Basic_Structure_and_Syntax/HelloWorld.java
+++ Basic_Structure_and_Syntax/HelloWorld.java
@@ -2,11 +2,26 @@ package Basic_Structure_and_Syntax;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Welcome to Java programming.");
-        System.out.println("I am Rho-Anne.");
+        System.out.println
+        (1234567890101111l);
+        System.out.println
+        ("Welcome to Java programming.");
+        System.out.println
+        ("I am Rho-Anne.");
         System.out.println("12- Peridot.");
-        System.out.println("Hello, Sir Seraspe!");
+        System.out.println
+        ("Hello, Sir Seraspe!");
+
+        System.out.println
+        ("Addition:" + (5 + 10) );   
+        System.out.println
+        ("Subtraction:" + (20 - 8) );
+        System.out.println
+        ("Multiplication:" + (4 * 6) );
+        System.out.println
+        ("Division:" + (15 / 3) );
+
+
         
     }
     
