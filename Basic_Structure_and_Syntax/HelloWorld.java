d86f1b6cbe26908fb7c7eeb6fd885e682e955770
Rho-Anne <rhoannecarlon607@gmail.com>
2026-06-17 13:02:36 +0800

    myfirstcommit

---
+++ Basic_Structure_and_Syntax/HelloWorld.java
@@ -0,0 +1,13 @@
+package Basic_Structure_and_Syntax;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java programming.");
+        System.out.println("I am Rho-Anne.");
+        System.out.println("12- Peridot.");
+        System.out.println("Hello, Sir Seraspe!");
+        
+    }
+    
+}
