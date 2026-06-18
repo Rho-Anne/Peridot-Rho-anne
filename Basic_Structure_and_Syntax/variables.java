4e84c06b9d775d010c5e4564d37fd4bb510391ad
Rho-Anne <rhoannecarlon607@gmail.com>
2026-06-18 14:42:52 +0800

    6/18/26

---
+++ Basic_Structure_and_Syntax/variables.java
@@ -0,0 +1,21 @@
+package Basic_Structure_and_Syntax;
+
+public class variables {
+    public static void main(String[] args) {
+        String name = "Rho-Anne";
+        int age = 17;
+        int score = 10;
+        System.out.println("Score:" + score);
+        System.out.println("Name:" + name);
+        System.out.println("Aage:" + age);
+        age= 25;
+        float height = 5/2;
+        System.out.println("Height:" + height);
+        char bloodtype = 'C';
+        System.out.println("BloodType:" + bloodtype);
+
+        
+
+
+    }
+}
---
+++ FACTSABOUTCAT/cats.java
@@ -0,0 +1,22 @@
+package FACTSABOUTCAT;
+
+public class cats {
+    public static void main(String[] args) {
+        String name = "Cats";
+        int averageLifespan = 15;
+        boolean isPet = true;
+        float averageWeight = 3.6F;
+        char bloodtype = 'A';
+
+        System.out.println(name + " have an average lifespan of " + averageLifespan + 
+        " years, have an average weight of " 
+        + averageWeight + " kg. Has a usually blood type of " + bloodtype + ".");
+
+        System.out.println("This is " + isPet + " because it came from a vet site.");
+    
+    }
+    
+}
+
+// The average weight of a household cat varies from 2.7 to 4.5 kg (6 to 10 pounds), although among non-pedigreed cats weights up to 
+// 12.7 kg (28 pounds) are not uncommon. Average lengths are 71.1 cm (28 inches) for males and 50.8 cm (20 inches) for females.
